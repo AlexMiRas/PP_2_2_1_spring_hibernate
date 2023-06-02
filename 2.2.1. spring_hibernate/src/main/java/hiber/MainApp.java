fbf2f5d8b2b7af004238e8b11d9a2afe60389334
Alexey Rasskazov <a9717993@yandex.ru>
2023-06-02 12:27:41 +0300

    Adding classes and interfaces CarDao, CarDaoImp, CarService and CarServiceImp

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -5,6 +5,8 @@ import hiber.dao.UserDao;
 import hiber.dao.UserDaoImp;
 import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
+import hiber.service.CarServiceImp;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -17,8 +19,9 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
 
-      User user1 = new User("Name1", "LastName1","mail1@mail.ru");
+      User user1 = new User("Name1", "LastName1", "mail1@mail.ru");
       User user2 = new User("Name2", "LastName2","mail2@mail.ru");
       User user3 = new User("Name3", "LastName3","mail3@mail.ru");
       User user4 = new User("Name4", "LastName4","mail4@mail.ru");
@@ -43,7 +46,8 @@ public class MainApp {
          System.out.println();
       }
 
-      System.out.println("------------------ \n" + userService.findUserByCar("AUDI", 5));
+      System.out.println("------------------ \n" + userService.findUserByCar("BMW", 2));
+      System.out.println("------------------ \n" + carService.listCar());
 
       context.close();
    }
